745a7ea5c4785f2a1b7bea2be43bc712699ceae6
RumbleFrog <xxlosttechxx@gmail.com>
2017-09-19 15:45:01 -0400

    Documentation
    Signed-off-by: RumbleFrog <xxlosttechxx@gmail.com>

--- src/Paint.java
+++ src/Paint.java
@@ -10,17 +10,26 @@ import java.util.Scanner;
  * @class Paint
  * @classdesc Prompt user for various parameters and outputs amount of paint needed
  */
-public class Paint {
+public class Paint
+{
+    /**
+     * PAINT_COVERAGE - Amount of square feet can a gallon of paint cover
+     * DOOR_COVERAGE - Amount of square feet a door takes up
+     * WINDOW_COVERAGE - Amount of square feet a window takes up
+     */
     private final static int PAINT_COVERAGE = 350, DOOR_COVERAGE = 20, WINDOW_COVERAGE = 15;
+    /**
+     * SCAN - Interprets console input
+     */
     private final static Scanner SCAN = new Scanner(System.in);
 
 
     /**
      * Primary paint method that does all the calculations
      * @param {Buffer} args
+     * @return {Void}
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int length, width, height, door, window;
         double totalSqFt, paintNeeded;
 
@@ -36,6 +45,11 @@ public class Paint {
         System.out.println("Total gallons of paint needed: " + (int) Math.ceil(paintNeeded) + " gallons.");
     }
 
+    /**
+     * Prompt user to enter an integer based on the question, and loop until integer only.
+     * @param {Buffer} question
+     * @return {Integer} User input
+     */
     private static int PromptInput(String question)
     {
         System.out.print(question);
