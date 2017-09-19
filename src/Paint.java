09d9fc10931c9b4253958f6000166a97e63879a8
RumbleFrog <admin@biturl.io>
2017-09-19 14:39:00 -0400

    Requesting dat
    Signed-off-by: RumbleFrog <admin@biturl.io>

--- src/Paint.java
+++ src/Paint.java
@@ -3,22 +3,42 @@
  * @since 19 September 2017
  */
 
+import java.util.Scanner;
+
 /**
  * Paint class
  * @class Paint
  * @classdesc Prompt user for various parameters and outputs amount of paint needed
  */
 public class Paint {
-    final static int COVERAGE = 350;
+    private final static int PAINT_COVERAGE = 350, DOOR_COVERAGE = 20, WINDOW_COVERAGE = 15;
+    private final static Scanner SCAN = new Scanner(System.in);
 
 
     public static void main(String[] args)
     {
+        int length, width, height, door, window;
+        double totalSqFt, paintNeeded;
 
+        length = PromptInput("Room length: ");
+        width = PromptInput("Room width: ");
+        height = PromptInput("Room height: ");
+        door = PromptInput("Amount of doors in the room: ");
+        window = PromptInput("Amount of windows in the room: ");
+
+        totalSqFt = (((width * height) * 2) + ((length * height) * 2)) - ((door * DOOR_COVERAGE) + (window * WINDOW_COVERAGE));
     }
 
-    public static int PromptInput(String[] question)
+    private static int PromptInput(String question)
     {
-        return 0;
+        while (!SCAN.hasNextInt()) {
+            System.out.flush();
+
+            System.out.print(question);
+
+            SCAN.next();
+        }
+
+        return SCAN.nextInt();
     }
 }
