22963fbd598fc42f49b7ee3040715f01e865f5f2
RumbleFrog <admin@biturl.io>
2017-09-19 14:08:41 -0400

    Declare functions
    Signed-off-by: RumbleFrog <admin@biturl.io>

---
+++ src/Paint.java
@@ -0,0 +1,24 @@
+/**
+ * @author Zhenyang Li
+ * @since 19 September 2017
+ */
+
+/**
+ * Paint class
+ * @class Paint
+ * @classdesc Prompt user for various parameters and outputs amount of paint needed
+ */
+public class Paint {
+    final static int COVERAGE = 350;
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static int PromptInput(String[] question)
+    {
+        return 0;
+    }
+}
